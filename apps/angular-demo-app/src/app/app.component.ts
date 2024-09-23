94823297fe2a4e65bb7cba9013c962b99cd69685
Mason Mark <mason@axhxrx.com>
2024-09-23 14:40:44 +0900

    feat(angular-demo-app): Use the JSR lib from legaprise app (via JSR's "NPM compatibility mode"[1])
    [1]: https://jsr.io/docs/npm-compatibility

--- apps/angular-demo-app/src/app/app.component.ts
+++ apps/angular-demo-app/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { sortedStringify } from '@axhxrx/json';
+
 @Component({
   standalone: true,
   imports: [RouterModule],
@@ -8,4 +10,19 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  useSortedStringify() {
+    const demoObject = {
+      radiation: false,
+      fire: true,
+      ice: false,
+      wind: false,
+      powers: ['fire', 'ice', 'wind', 'radiation'],
+    };
+
+    const before = JSON.stringify(demoObject, null, 2);
+    const after = sortedStringify(demoObject, 2);
+
+    return`BEFORE:\n${before}\n\nAFTER:\n${after}`;
+  }
+}
