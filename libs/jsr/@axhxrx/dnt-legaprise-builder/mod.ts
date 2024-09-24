fe6f8e72492ce6d7a4cbb42226080d51d5e38bbb
Mason Mark <mason@axhxrx.com>
2024-09-24 22:19:06 +0900

    EXAMPLE: Seems like should work with DNT but does not

--- libs/jsr/@axhxrx/dnt-legaprise-builder/mod.ts
+++ libs/jsr/@axhxrx/dnt-legaprise-builder/mod.ts
@@ -1,18 +1,18 @@
 // ex. scripts/build_npm.ts
-import { build, emptyDir } from "jsr:@deno/dnt";
-import { parse } from "https://deno.land/std@0.201.0/flags/mod.ts";
+import { build, emptyDir } from 'jsr:@deno/dnt';
+import { parse } from 'https://deno.land/std@0.201.0/flags/mod.ts';
 
 function parseArgs() {
   // Parse the command-line arguments
   const args = parse(Deno.args, {
-    string: ["input", "output"], // Specify that 'input' and 'output' expect string values
+    string: ['input', 'output'], // Specify that 'input' and 'output' expect string values
     alias: {
-      i: "input", // Map '-i' to '--input'
-      o: "output", // Map '-o' to '--output'
+      i: 'input', // Map '-i' to '--input'
+      o: 'output', // Map '-o' to '--output'
     },
     default: {
-      input: "",
-      output: "",
+      input: '',
+      output: '',
     },
   });
 
@@ -25,29 +25,36 @@ function parseArgs() {
 export async function main() {
   const { input, output } = parseArgs();
   if (!input || !output) {
-    console.error("Please provide both input and output paths.");
+    console.error('Please provide both input and output paths.');
     Deno.exit(1);
   }
 
   await emptyDir(output);
 
   await build({
+    // @masonmark 2024-09-24: I feel like this SHOULD be enough to make DNT see that ./libs/@axhxrx/jsr/ts/mod.ts is v.0.1.2, and therefore "jsr:@axhxrx/ts@0.1.2" should resolve to the local monorepo. But it doesn'r
+    importMap: './deno.jsonc',
+
+    //So, try this:
+    shims: {
+      deno: true,
+      custom: [{
+        globalNames: ['jsr:@axhxrx/ts@0.1.2'],
+        module: './libs/@axhxrx/ts/mod.ts',
+      }],
+    },
+
     //   typeCheck: false,
     // declaration: true,
-      scriptModule: false,
+    scriptModule: false,
     entryPoints: [input],
     outDir: output,
-    shims: {
-      // see JS docs for overview and more options
-      deno: true,
-    },
     package: {
       // package.json properties
-      name: "@legaprise/json",
+      name: '@legaprise/json',
       version: Deno.args[0],
-      description: "Your package.",
-      license: "MIT",
-   
+      description: 'Your package.',
+      license: 'MIT',
     },
     // postBuild() {
     //   // steps to run after building and before running the tests
